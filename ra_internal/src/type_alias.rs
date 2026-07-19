1c80686076fe7e381982c54dfb283f67b205d7c8
anatawa12 <anatawa12@icloud.com>
2026-07-19 17:21:37 +0900

    fix: trait associated type bounds not proceed correctly

--- ra_internal/src/type_alias.rs
+++ ra_internal/src/type_alias.rs
@@ -1,13 +1,28 @@
+use crate::TypeExt;
 use crate::internal::{ParsedProjection, parse_bounds_for};
+use hir::HasContainer;
 use hir::db::HirDatabase;
 
-pub trait TypeAliasExt {
-    fn bounds<'db>(&self, db: &'db dyn HirDatabase) -> Vec<(hir::Trait, Vec<hir::Type<'db>>)>;
+pub trait TypeAliasExt: Copy {
+    fn bounds<'db>(self, db: &'db dyn HirDatabase) -> Vec<(hir::Trait, Vec<hir::Type<'db>>)>;
 }
 
 impl TypeAliasExt for hir::TypeAlias {
-    fn bounds<'db>(&self, db: &'db dyn HirDatabase) -> Vec<(hir::Trait, Vec<hir::Type<'db>>)> {
-        match parse_bounds_for([], &self.ty(db), db) {
+    fn bounds<'db>(self, db: &'db dyn HirDatabase) -> Vec<(hir::Trait, Vec<hir::Type<'db>>)> {
+        let db_0 = match self.container(db) {
+            hir::ItemContainer::Trait(t) => {
+                let self_ty = hir::GenericDef::Trait(t)
+                    .params(db)
+                    .into_iter()
+                    .flat_map(|x| variant_or_none!(x, hir::GenericParam::TypeParam))
+                    .next()
+                    .unwrap();
+                self_ty.ty(db).new_associated_type(&[self], db)
+            }
+            _ => unreachable!(),
+        };
+
+        match parse_bounds_for([], &db_0, db) {
             ParsedProjection::NoBounds => vec![],
             ParsedProjection::Projection(_) => panic!(),
             ParsedProjection::Traits(traits) => traits,
