89ffcb485e567d52376bca548ade20c13e3497ae
anatawa12 <anatawa12@icloud.com>
2026-07-27 01:31:37 +0900

    as_ref of box

--- src/codegen/function_resolution.rs
+++ src/codegen/function_resolution.rs
@@ -157,6 +157,18 @@ impl<'db> CodeGenerator<'db> {
             };
         }
 
+        if f.name(self.db).as_str() == "as_ref"
+            && let hir::ItemContainer::Impl(impl_) = f.container(self.db)
+            && let self_ty = impl_.self_ty(self.db)
+            && let Some(adt) = self_ty.as_adt()
+            && let hir::Adt::Struct(struct_) = adt
+            && Some(struct_) == lang_items.OwnedBox()
+        {
+            return ResolvedFunction::OmitCall {
+                comment: "/*omit Box::as_ref method*/".into(),
+            };
+        }
+
         if f.name(self.db).as_str() == "new"
             && let hir::ItemContainer::Impl(impl_) = f.container(self.db)
             && let self_ty = impl_.self_ty(self.db)
