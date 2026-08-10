3459099e46d0f897e853c9ea28273d0eea3310f8
anatawa12 <anatawa12@icloud.com>
2026-08-10 14:34:07 +0900

    refactor: how define lang_items

--- ra_internal/src/lang_items.rs
+++ ra_internal/src/lang_items.rs
@@ -4,162 +4,6 @@ use base_db::{Crate, CrateOrigin, LangCrateOrigin, all_crates};
 use hir_def::lang_item::{LangItems as HirDefLangItems, lang_items};
 use hir_ty::db::HirDatabase;
 
-#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
-pub struct LangItems {
-    inner: HirDefLangItems,
-    send: Option<hir::Trait>,
-    into: Option<hir::Trait>,
-    cow: Option<hir::Enum>,
-    cow_owned: Option<hir::EnumVariant>,
-    cow_borrowed: Option<hir::EnumVariant>,
-    os_str: Option<hir::Struct>,
-    os_string: Option<hir::Struct>,
-    into_iterator: Option<hir::Trait>,
-    result: Option<hir::Enum>,
-    unreachable: Option<hir::Macro>,
-    panic: Option<hir::Macro>,
-    matches: Option<hir::Macro>,
-    ready: Option<hir::Macro>,
-    vec: Option<hir::Macro>,
-    pin: Option<hir::Macro>,
-}
-
-impl LangItems {
-    pub fn new(db: &dyn HirDatabase, krate: hir::Crate) -> &Self {
-        #[salsa_macros::tracked(returns(ref))]
-        fn new(db: &dyn HirDatabase, krate: Crate) -> LangItems {
-            let send: Option<hir::Trait>;
-            let into: Option<hir::Trait>;
-            let cow: Option<hir::Enum>;
-            let cow_owned: Option<hir::EnumVariant>;
-            let cow_borrowed: Option<hir::EnumVariant>;
-            let os_str: Option<hir::Struct>;
-            let os_string: Option<hir::Struct>;
-            let into_iterator: Option<hir::Trait>;
-            let result: Option<hir::Enum>;
-            let unreachable: Option<hir::Macro>;
-            let panic: Option<hir::Macro>;
-            let matches: Option<hir::Macro>;
-            let ready: Option<hir::Macro>;
-            let vec: Option<hir::Macro>;
-            let pin: Option<hir::Macro>;
-
-            {
-                trait ItemInNsConvert: Sized {
-                    fn into_ns(value: hir::ItemInNs) -> Option<Self>;
-                }
-
-                macro_rules! item_in_ns_convert_impl {
-                    ($ty: path as ($($convert: path),+)) => {
-                        impl ItemInNsConvert for $ty {
-                            fn into_ns(value: hir::ItemInNs) -> Option<Self> {
-                                $(let value = variant_or_none!(value, $convert)?;)+
-                                Some(value)
-                            }
-                        }
-                    };
-                }
-
-                item_in_ns_convert_impl!(
-                    hir::Trait as (hir::ItemInNs::Types, hir::ModuleDef::Trait)
-                );
-                item_in_ns_convert_impl!(
-                    hir::Enum as (hir::ItemInNs::Types, hir::ModuleDef::Adt, hir::Adt::Enum)
-                );
-                item_in_ns_convert_impl!(
-                    hir::Struct as (hir::ItemInNs::Types, hir::ModuleDef::Adt, hir::Adt::Struct)
-                );
-                item_in_ns_convert_impl!(
-                    hir::EnumVariant as (hir::ItemInNs::Types, hir::ModuleDef::EnumVariant)
-                );
-                item_in_ns_convert_impl!(hir::Macro as (hir::ItemInNs::Macros));
-
-                macro_rules! resolve_item {
-                    ($crate_:ident ::$($path:ident)::+ as $ty: ty) => {{
-                        {
-                            #![allow(unused)]
-                            extern crate alloc;
-                            use $crate_::$($path)::+;
-                        }
-                        $crate_.and_then(|crate_| crate_
-                            .root_module(db)
-                            .resolve_mod_path(
-                                db,
-                                [$( ::hir::Name::new_root(stringify!($path))),+ ],
-                            )
-                            .into_iter()
-                            .flatten()
-                            .flat_map(|x| <$ty as ItemInNsConvert>::into_ns(x))
-                            .next()
-                        )
-                    }};
-                }
-
-                let all_crates = all_crates(db);
-                macro_rules! find_crate {
-                    (|$origin:ident| $expr: expr) => {
-                        all_crates
-                            .iter()
-                            .copied()
-                            .find(|&krate| {
-                                let $origin = &krate.data(db).origin;
-                                $expr
-                            })
-                            .map(hir::Crate::from)
-                    };
-                }
-
-                let core = find_crate!(|origin| {
-                    matches!(origin, CrateOrigin::Lang(LangCrateOrigin::Core))
-                });
-                let alloc = find_crate!(|origin| {
-                    matches!(origin, CrateOrigin::Lang(LangCrateOrigin::Alloc))
-                });
-                let std = find_crate!(|origin| {
-                    matches!(origin, CrateOrigin::Lang(LangCrateOrigin::Std))
-                });
-
-                send = resolve_item!(core::marker::Send as hir::Trait);
-                into = resolve_item!(core::convert::Into as hir::Trait);
-                cow = resolve_item!(alloc::borrow::Cow as hir::Enum);
-                cow_borrowed = resolve_item!(alloc::borrow::Cow::Borrowed as hir::EnumVariant);
-                cow_owned = resolve_item!(alloc::borrow::Cow::Owned as hir::EnumVariant);
-                os_str = resolve_item!(std::ffi::OsStr as hir::Struct);
-                os_string = resolve_item!(std::ffi::OsString as hir::Struct);
-                into_iterator = resolve_item!(std::iter::IntoIterator as hir::Trait);
-                result = resolve_item!(std::result::Result as hir::Enum);
-                unreachable = resolve_item!(core::unreachable as hir::Macro);
-                panic = resolve_item!(std::panic as hir::Macro);
-                matches = resolve_item!(core::matches as hir::Macro);
-                ready = resolve_item!(core::task::ready as hir::Macro);
-                vec = resolve_item!(alloc::vec as hir::Macro);
-                pin = resolve_item!(core::pin::pin as hir::Macro);
-            }
-
-            LangItems {
-                inner: lang_items(db, krate).clone(),
-                send,
-                into,
-                cow,
-                cow_owned,
-                cow_borrowed,
-                os_str,
-                os_string,
-                into_iterator,
-                result,
-                unreachable,
-                panic,
-                matches,
-                ready,
-                vec,
-                pin,
-            }
-        }
-
-        new(db, krate.base())
-    }
-}
-
 macro_rules! lang_item_wrapper {
     (
         struct $name:ident {
@@ -172,6 +16,142 @@ macro_rules! lang_item_wrapper {
             })*
         }
     };
+}
+
+trait ItemInNsConvert: Sized {
+    fn from_item_in_ns(value: hir::ItemInNs) -> Option<Self>;
+}
+
+macro_rules! item_in_ns_convert_impl {
+    ($ty: path as ($($convert: path),+)) => {
+        impl ItemInNsConvert for $ty {
+            fn from_item_in_ns(value: hir::ItemInNs) -> Option<Self> {
+                $(let value = variant_or_none!(value, $convert)?;)+
+                Some(value)
+            }
+        }
+    };
+}
+
+item_in_ns_convert_impl!(hir::Trait as (hir::ItemInNs::Types, hir::ModuleDef::Trait));
+item_in_ns_convert_impl!(hir::Enum as (hir::ItemInNs::Types, hir::ModuleDef::Adt, hir::Adt::Enum));
+item_in_ns_convert_impl!(
+    hir::Struct as (hir::ItemInNs::Types, hir::ModuleDef::Adt, hir::Adt::Struct)
+);
+item_in_ns_convert_impl!(hir::EnumVariant as (hir::ItemInNs::Types, hir::ModuleDef::EnumVariant));
+item_in_ns_convert_impl!(hir::Macro as (hir::ItemInNs::Macros));
+
+macro_rules! resolve_item {
+    ($crate_:ident ::$($path:ident)::+, $db: ident) => {{
+        $crate_.and_then(|crate_| crate_
+            .root_module($db)
+            .resolve_mod_path(
+                $db,
+                [$( ::hir::Name::new_root(stringify!($path))),+ ],
+            )
+            .into_iter()
+            .flatten()
+            .flat_map(ItemInNsConvert::from_item_in_ns)
+            .next()
+        )
+    }};
+}
+
+macro_rules! find_crate {
+    ($all_crates: ident, $db: ident, |$origin:ident| $expr: expr) => {
+        $all_crates
+            .iter()
+            .copied()
+            .find(|&krate| {
+                let $origin = &krate.data($db).origin;
+                $expr
+            })
+            .map(hir::Crate::from)
+    };
+}
+
+macro_rules! our_lang_items {
+    (
+        {
+            extern crate $core: ident;
+            extern crate $alloc: ident;
+            extern crate $std: ident;
+            $(extern crate $krate: ident;)*
+        }
+        $($name: ident = $($path: ident)::* as $ty: ident;)*
+    ) => {
+        #[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
+        pub struct LangItems {
+            inner: HirDefLangItems,
+            $($name: Option<hir::$ty>,)*
+        }
+
+        impl LangItems {
+
+            pub fn new(db: &dyn HirDatabase, krate: hir::Crate) -> &Self {
+                #[salsa_macros::tracked(returns(ref))]
+                fn new(db: &dyn HirDatabase, krate: Crate) -> LangItems {
+                   new_impl(db, krate)
+                }
+                fn new_impl(db: &dyn HirDatabase, krate: Crate) -> LangItems {
+                    let all_crates = all_crates(db);
+
+                    let $core = find_crate!(all_crates, db, |origin| matches!(origin, CrateOrigin::Lang(LangCrateOrigin::Core)));
+                    let $alloc = find_crate!(all_crates, db, |origin| matches!(origin, CrateOrigin::Lang(LangCrateOrigin::Alloc)));
+                    let $std = find_crate!(all_crates, db, |origin| matches!(origin, CrateOrigin::Lang(LangCrateOrigin::Std)));
+
+                    $(let $krate = find_crate!(all_crates, db, |origin| {
+                        matches!(origin, CrateOrigin::Library{ name: name, .. } if name.as_str() == stringify!($krate))
+                    });)*
+
+                    LangItems {
+                        inner: lang_items(db, krate).clone(),
+
+                        $($name: resolve_item!($($path)::*, db),)*
+                    }
+                }
+
+                new(db, krate.base())
+            }
+        }
+
+        impl LangItems {
+            $(
+                pub fn $name(&self) -> Option<hir::$ty> {
+                    self.$name
+                }
+            )*
+        }
+    };
+}
+
+our_lang_items! {
+    {
+        extern crate core;
+        extern crate alloc;
+        extern crate std;
+    }
+
+    // core
+    ready = core::task::ready as Macro;
+    pin = core::pin::pin as Macro;
+    unreachable = core::unreachable as Macro;
+    matches = core::matches as Macro;
+    Send = core::marker::Send as Trait;
+    Into = core::convert::Into as Trait;
+
+    // alloc
+    vec = alloc::vec as Macro;
+    Cow = alloc::borrow::Cow as Enum;
+    CowBorrowed = alloc::borrow::Cow::Borrowed as EnumVariant;
+    CowOwned = alloc::borrow::Cow::Owned as EnumVariant;
+
+    // std
+    panic = std::panic as Macro;
+    OsStr = std::ffi::OsStr as Struct;
+    OsString = std::ffi::OsString as Struct;
+    IntoIterator = std::iter::IntoIterator as Trait;
+    Result = std::result::Result as Enum;
 }
 
 lang_item_wrapper! {
@@ -205,67 +185,5 @@ lang_item_wrapper! {
 
         Debug: Trait,
         Hash: Trait,
-    }
-}
-
-impl LangItems {
-    pub fn Send(&self) -> Option<hir::Trait> {
-        self.send
-    }
-
-    pub fn Into(&self) -> Option<hir::Trait> {
-        self.into
-    }
-
-    pub fn Cow(&self) -> Option<hir::Enum> {
-        self.cow
-    }
-
-    pub fn CowOwned(&self) -> Option<hir::EnumVariant> {
-        self.cow_owned
-    }
-
-    pub fn CowBorrowed(&self) -> Option<hir::EnumVariant> {
-        self.cow_borrowed
-    }
-
-    pub fn OsStr(&self) -> Option<hir::Struct> {
-        self.os_str
-    }
-
-    pub fn OsString(&self) -> Option<hir::Struct> {
-        self.os_string
-    }
-
-    pub fn IntoIterator(&self) -> Option<hir::Trait> {
-        self.into_iterator
-    }
-
-    pub fn Result(&self) -> Option<hir::Enum> {
-        self.result
-    }
-
-    pub fn unreachable(&self) -> Option<hir::Macro> {
-        self.unreachable
-    }
-
-    pub fn panic(&self) -> Option<hir::Macro> {
-        self.panic
-    }
-
-    pub fn matches(&self) -> Option<hir::Macro> {
-        self.matches
-    }
-
-    pub fn ready(&self) -> Option<hir::Macro> {
-        self.ready
-    }
-
-    pub fn vec(&self) -> Option<hir::Macro> {
-        self.vec
-    }
-
-    pub fn pin(&self) -> Option<hir::Macro> {
-        self.pin
     }
 }
